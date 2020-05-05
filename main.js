8904c030fe9404267836ba64cc58b0ec9c56b29c
arthur9010 <arthur.alves@redfox.tech>
2020-05-05 12:19:50 -0300

    REFAC: remove express-load and add consign

--- main.js
+++ main.js
@@ -3,18 +3,20 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParse = require('body-parser')
-const load = require('express-load')
+const load = require('consign')
 
 const port = process.env.API_PORT
 
 const app = express()
 
-load('controllers').into(app)
+load({cwd: 'src'}).include('controllers').then('routers').into(app)
 
 app.use(cors())
 app.use(morgan('dev'))
 app.use(bodyParse.urlencoded({extended: false}))
 app.use(bodyParse.json())
 app.listen(port, () => console.log('API RODANDO NA PORTA ', port))
+
+module.exports = app
 
 
